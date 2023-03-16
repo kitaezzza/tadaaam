f20afa2d193b8d8a4e2010c334010e7bda75644a
kitaezzza <123646686+kitaezzza@users.noreply.github.com>
2023-03-16 19:36:48 +0400

    Добавьте файлы проекта.

---
+++ tadaaam/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using tadaaam.Windows;
+
+namespace tadaaam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Champion> _data = new ObservableCollection<Champion>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_del_Click(object sender, RoutedEventArgs e)
+        {
+            Champion del = ((Champion)d_grid.SelectedItem);
+            d_grid.Items.Remove(del);
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            AddData add = new AddData();
+            add.Show();
+        }
+
+        private void btn_openDialog_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Filter = "Json files (*.json)|*.json";
+            oFile.ShowDialog();
+            if (string.IsNullOrEmpty(oFile.FileName))
+            {
+                return;
+            }
+            string json = File.ReadAllText(oFile.FileName);
+
+            try
+            {
+                _data = JsonSerializer.Deserialize<ObservableCollection<Champion>>(json);
+                foreach (Champion champ in _data)
+                {
+                    d_grid.Items.Add(champ);
+                }
+            }
+            catch (System.Text.Json.JsonException)
+            {
+                MessageBox.Show("В данном файле отсутствуют компоненты нужного формата");
+            }
+        }
+    }
+}
---
+++ tadaaam/Windows/AddData.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tadaaam.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddData.xaml
+    /// </summary>
+    public partial class AddData : Window
+    {
+        public ObservableCollection<Champion> _data = new ObservableCollection<Champion>();
+
+        Guid Guid = Guid.NewGuid();
+        public AddData()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_saveData_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sFile = new SaveFileDialog();
+            string json = JsonSerializer.Serialize<ObservableCollection<Champion>>(_data);
+
+            sFile.Filter = "Json files (*.json)|*.json";
+
+            sFile.ShowDialog();
+            if (string.IsNullOrEmpty(sFile.FileName))
+                return;
+            File.WriteAllText(sFile.FileName, json);
+        }
+
+        private void btn_addData_Click(object sender, RoutedEventArgs e)
+        {
+            Champion champion = new Champion()
+            { 
+                Name = tb_name.Text,
+                Class= tb_class.Text,
+                Position = tb_position.Text
+            };
+            _data.Add(champion);
+        }
+    }
+}
